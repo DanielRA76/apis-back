8a7469f8809755a5b6c95dc35bdb98bfaa4d0bf0
DANIEL RODRIGUEZ ALFONSO <daniel.rodriguez.alfonso@gmail.com>
2019-04-02 01:43:53 +0200

    Conectamos con variables de entorno.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -16,19 +18,48 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+
 app.use(cookieParser());
+//para servir ficheros estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Variables globales de template
+app.locals.titulo = 'BBQ API';
+
+/**
+ * Conectamos a la base de datos
+ * y registramos los modelos
+ */
+require('./lib/connectMongoose');
+//require('./models/Recetas');
+
+
+
+/**
+ * Rutas de mi aplicación Web
+ */
+app.use('/',      require('./routes/index'));
+//app.use('/users', require('./routes/users'));
+
+/** 
+app.get('/login', loginController.index);
+app.post('/login', loginController.post);
+app.get('/logout', loginController.logout);
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
